6ffd1db23db417467363491f6a1a28fa236c4caf
MrKolzy <kolzykun@gmail.com>
2025-09-01 16:04:47 +0200

    small fix

--- include/diaxx/constants.hpp
+++ include/diaxx/constants.hpp
@@ -20,9 +20,9 @@ namespace constants
 	inline constexpr bool g_enable_validation_layers { true };
 #endif
 
-	inline constexpr std::array<const char*, 4> g_device_extensions {
+	inline constexpr std::array<const char*, 5> g_device_extensions {
 		"VK_KHR_swapchain",	"VK_KHR_spirv_1_4", "VK_KHR_synchronization2",
-		"VK_KHR_create_renderpass2" };
+		"VK_KHR_create_renderpass2", "VK_KHR_shader_draw_parameters" };
 
 	inline constexpr vk::DebugUtilsMessageSeverityFlagsEXT severity_flags(
 		vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
